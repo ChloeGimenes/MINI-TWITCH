585069f9ae8fc8eceaeff053f65c7c934c351b6e
CHLOE GIMENES <chloegimenes@gmail.com>
2020-06-19 15:05:26 +0200

    upgrading api authorization

--- src/api.js
+++ src/api.js
@@ -2,8 +2,19 @@ import axios from 'axios';
 
 let api = axios.create({
     headers: {
-        'Client-ID': 'vwbdcy6y43agee5wsg71p45c5ah7d5'
+        'Client-ID': 'msqa4m7fnzhvv2d7a5lwea6q1vsmnp',
+        'Authorization': 'Bearer b0lpzxh1mbpqyr6p1j415bnb7c92p3'
     }
 })
+
+/*
+
+    CLIENT_ID = msqa4m7fnzhvv2d7a5lwea6q1vsmnp
+    REDIRECT = 'http://192.168.1.51/'
+    LIEN AUTH = https://id.twitch.tv/oauth2/authorize?client_id={CLIENT_ID}&redirect_uri={REDIRECT}&response_type=token
+
+    LIEN REMPLI = https://id.twitch.tv/oauth2/authorize?client_id=msqa4m7fnzhvv2d7a5lwea6q1vsmnp&redirect_uri=http://192.168.1.51/&response_type=token
+
+*/
 
 export default api;
